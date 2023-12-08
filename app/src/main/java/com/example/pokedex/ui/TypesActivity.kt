10735c1b67f1ee411b4f6a8889857475fab20790
LuidyEnrico <80763934+Luidyenrico@users.noreply.github.com>
2023-12-08 03:40:40 -0300

    Att pagina tipos

--- app/src/main/java/com/example/pokedex/ui/TypesActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/TypesActivity.kt
@@ -1,12 +1,25 @@
 package com.example.pokedex.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pokedex.R
 
 class TypesActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_types)
+
+        val botaoRetornar: ImageButton = findViewById(R.id.botaoRetornar)
+
+        botaoRetornar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                val intent = Intent(this@TypesActivity, InicialActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
