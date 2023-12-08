ab223a61627ff52fb6b0ac882699b4a948465fdc
LuidyEnrico <80763934+Luidyenrico@users.noreply.github.com>
2023-12-08 18:49:18 -0300

    battle works

--- app/src/main/java/com/example/pokedex/ui/BattleActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/BattleActivity.kt
@@ -7,6 +7,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,7 @@ import com.example.pokedex.R
 
 class BattleActivity : AppCompatActivity() {
 
+    private lateinit var resultadoImagem: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_battle)
@@ -28,6 +30,7 @@ class BattleActivity : AppCompatActivity() {
 
         val spinner1: Spinner = findViewById(R.id.spinner1)
         val spinner2: Spinner = findViewById(R.id.spinner2)
+        resultadoImagem = findViewById(R.id.resultadoImagem)
 
         // Array de opções para os Spinners
         val opcoes = arrayOf("NORMAL", "FIRE", "GRASS", "WATER", "ELECTRIC", "ICE", "GROUND", "FLYING", "POISON",
@@ -69,51 +72,77 @@ class BattleActivity : AppCompatActivity() {
 
             // Verificação de empate
             if (selecaoSpinner1.equals(selecaoSpinner2, ignoreCase = true)) {
-                Toast.makeText(this, "Empate!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "DRAW", Toast.LENGTH_SHORT).show()
             } else {
                 // Determinar o vencedor e exibir o resultado
                 val vencedor = determinarVencedor(selecaoSpinner1, selecaoSpinner2)
                 exibirResultado(vencedor)
             }
         }
+
     }
 
     // Lógica para determinar o vencedor
     private fun determinarVencedor(tipo1: String, tipo2: String): String {
         if (tipo1 == tipo2) {
-            return "Empate!"
+            return "DRAW"
         }
 
-        return when (tipo1) {
-            "FIRE" -> when (tipo2) {
-                "WATER", "GRASS" -> "WATER"
-                "FLYING" -> "FIRE"
-                else -> tipo2
-            }
-            "WATER" -> when (tipo2) {
-                "FIRE", "GROUND" -> "WATER"
-                "FLYING" -> "FLYING"
-                else -> tipo2
-            }
-            "GRASS" -> when (tipo2) {
-                "FIRE", "GROUND" -> "GRASS"
-                "FLYING" -> "FLYING"
-                else -> tipo2
-            }
-            "FLYING" -> when (tipo2) {
-                "FIRE" -> "FIRE"
-                "WATER", "GRASS" -> "FLYING"
-                else -> tipo2
-            }
-            else -> "Resultado Indefinido"
+        val vantagensTipo1 = obterVantagens(tipo1)
+        val vantagensTipo2 = obterVantagens(tipo2)
+
+        if (vantagensTipo1.contains(tipo2)) {
+            return tipo1
+        } else if (vantagensTipo2.contains(tipo1)) {
+            return tipo2
+        } else {
+            return "DRAW"
+        }
+    }
+
+    private fun obterVantagens(tipo: String): List<String> {
+        return when (tipo) {
+            "NORMAL" -> emptyList()
+            "GRASS" -> listOf("GROUND", "ROCK", "WATER")
+            "FIRE" -> listOf("BUG", "GRASS", "ICE", "STEEL")
+            "WATER" -> listOf("FIRE", "GROUND", "ROCK")
+            "ELECTRIC" -> listOf("WATER", "FLYING")
+            "FLYING" -> listOf("BUG", "FIGHTING", "GRASS")
+            "ICE" -> listOf("DRAGON", "FLYING", "GRASS", "GROUND")
+            "ROCK" -> listOf("BUG", "FIRE", "FLYING", "ICE")
+            "GROUND" -> listOf("ELECTRIC", "FIRE", "POISON", "ROCK", "STEEL")
+            "STEEL" -> listOf("FAIRY", "ICE", "ROCK", "POISON")
+            "FIGHTING" -> listOf("DARK", "ICE", "NORMAL", "ROCK", "STEEL")
+            "DARK" -> listOf("GHOST", "PSYCHIC")
+            "PSYCHIC" -> listOf("FIGHTING", "POISON")
+            "POISON" -> listOf("FAIRY", "GRASS")
+            "BUG" -> listOf("DARK", "GRASS", "PSYCHIC")
+            "FAIRY" -> listOf("DARK", "DRAGON", "FIGHTING")
+            "GHOST" -> listOf("PSYCHIC")
+            "DRAGON" -> emptyList()
+            else -> emptyList()
         }
     }
 
     // Lógica para exibir o resultado
     private fun exibirResultado(vencedor: String) {
-        // Adicione a lógica de exibição de resultado aqui
-        // Por enquanto, vamos apenas exibir uma mensagem com o tipo vencedor
-        Toast.makeText(this, "Vencedor: $vencedor", Toast.LENGTH_SHORT).show()
+        // Verifica se o vencedor é "DRAW"
+        val mensagem = if (vencedor == "DRAW") {
+            "DRAW"
+        } else {
+            "$vencedor WINS"
+        }
+
+        // Exibe a mensagem
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show()
+
+        if (vencedor != "DRAW") {
+            val resourceId = resources.getIdentifier("${vencedor.toLowerCase()}_img", "drawable", packageName)
+            resultadoImagem.setImageResource(resourceId)
+            resultadoImagem.visibility = View.VISIBLE
+        } else {
+            resultadoImagem.visibility = View.INVISIBLE
+        }
     }
 }
 
